7f695f307fca7f3674be520016286a86a4dd775d
marcink11 <s232429@student.dtu.dk>
2024-04-25 02:48:53 +0200

    Add check_belief function

--- belief_revision_agent.py
+++ belief_revision_agent.py
@@ -17,12 +17,12 @@ class BeliefRevisionAgent:
         solver =  Solver()
         
         # Add all beliefs to the solver
-        for clause in self.knowledge_base:
+        for clause in self.belief_base.beliefs:
             solver.add_clause(clause)
         solver.add_clause(new_clause)
         
         # Check if the new knowledge is consistent
-        if solver.solve():
+        if solver.check_clauses():
             # If consistent, add the new belief
             self.knowledge_base.append(new_clause)
         else:
@@ -40,15 +40,28 @@ class BeliefRevisionAgent:
         pass
 
 
-    def check_belief(self, clause):
-        """Method for checking logical entailment (e.g., resolution-based)"""
+    def check_belief(self, pos_clause):
+        '''
+        Method for checking logical entailment (e.g., resolution-based)
+        
+        pos_clause - clause to check for entailment, not negated (will be negated in the method),
+                     it should be in the form compliant with sympy library, since it will be
+                     converted to our custom CNF.
+        '''
         solver = Solver()
-        for clause in self.knowledge_base:
+        for clause in self.belief_base.beliefs:
             solver.add_clause(clause)
         
-        if solver.check_clauses():
-            print(f"Belief is entailed: {clause}")
+        pos_clause = self.belief_base.format_symopy_to_our_format(pos_clause)
+        neg_clause = []
+        for literal in pos_clause:
+            neg_clause.append([(not literal[0][0], literal[0][1])])
+
+        solver.add_clause(neg_clause)
+        
+        if solver.check_clauses(): # contradiction found for neg_clause, so the belief is entailed for pos_clause
+            print(f"Belief is entailed: {pos_clause}")
             return True
         else:
-            print(f"Belief is not entailed: {clause}")
+            print(f"Belief is not entailed: {pos_clause}")
             return False
