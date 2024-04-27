0fe4fbe9dc0e7e660e0b2bf0963052c132098adf
Marcel Zelent <79370135+MarcelZelent@users.noreply.github.com>
2024-04-27 14:01:53 +0200

    Finished structure for success, inclusion, consistency and extensionality

--- agms.py
+++ agms.py
@@ -10,55 +10,77 @@ class AGM:
     
     def agm_success(self, belief_base, phi):
 
-        for belief in belief_base:
-            if belief == phi:
-                print("Success")
-                break
-        else:
-            print("Success postulate not met")
+        belief_base_copy = belief_base.copy()
+        belief_base_copy.append(phi)
+        result = self.find_phi_in_base(belief_base_copy, phi)
+        return result
 
     def agm_inclusion(self, belief_base, phi):
 
-        # belief_base_copy = belief_base
-        # base_expanded = belief_base.add_belief(belief_base_copy, phi)
-        # base_revised = belief_base.revision(belief_base_copy, phi)
-
-         # if base_expanded == base_revised:
-        #     print("Inclusion success")
-        # else:
-        #     print("Inclusion failure")
-
         base_expanded = belief_base.copy()
         base_revised = belief_base.copy()
-        # base_expanded.append(phi)
+
         base_revised.remove(2)
         base_revised.append(phi)
-        # base_revised.append(phi)
+        base_expanded.append(phi)
 
-       
+        result = self.compare_bases(base_revised, base_expanded)
 
-        print(base_expanded)
-        print(base_revised)
+        return result
 
-        for belief_rev in base_revised:
-            for belief_exp in base_expanded:
-                if belief_rev == belief_exp:
-                    break
-            else:
-                print("Inclusion failed")
-                break
+    def agm_vacuity(self, belief_base, phi):
 
+        belief_base_copy = belief_base.copy()
+        phi_copy = phi
+        not_phi = 6
+
+        phi_found = self.find_phi_in_base(belief_base_copy, not_phi)
+
+        if phi_found:
+            
+            # print("Vacuity not applicable")
+            return None
+        
+        else:
+            base_expanded = belief_base_copy.copy()
+            base_revised = belief_base_copy.copy()
+
+            base_revised.remove(2)
+            base_revised.append(phi_copy)
+
+            base_expanded.append(phi_copy)
+
+            result = self.compare_bases(base_revised, base_expanded)
+
+            return result
+        
+    def agm_consistency(self, belief_base, phi):
+        return 
+    
+    def agm_extensionality(self, belief_base, phi):
+
+        phi_copy = phi
+        phi_check = phi
+
+        base1_revised = belief_base.copy()
+        base2_revised = belief_base.copy()
+
+        base1_revised.remove(2)
+        base1_revised.append(phi_copy)
+
+        base2_revised.remove(2)
+        base2_revised.append(phi_check)
+
+        result = self.compare_bases(base1_revised, base2_revised)
+
+        return result
+
+        
     '''''
 
     pseudo code for the rest
 
-    def agm_vacuity(self, belief_base, phi):
-
-        if Not(phi) is not in belief_base:
-            result = agm_inclusion
-            success
-        else:
-            failure
+  
 
     def agm_consistency(self, belief_base, phi):
         
@@ -77,6 +99,37 @@ class AGM:
 
     '''
 
+    def compare_bases(self, base1, base2):
+
+        # base 1 = revised base
+        # base 2 = expanded base
+
+        base1_copy = base1.copy()
+        base2_copy = base2.copy()
+
+        state = "failure"
+
+        for belief1 in base1:
+            for belief2 in base2:
+                if belief1 == belief2:
+                    state = "success"
+                    break
+            else:
+                state = "failure"
+                break
+        if state == "success":
+            return True
+        else:
+            return False
+    
+    def find_phi_in_base(self, belief_base, phi):
+
+        for belief in belief_base:
+            if belief == phi:
+                return True
+        else:
+            return False
+
 
 def main():
     belief_base_test = [1, 2, 3]
@@ -84,7 +137,19 @@ def main():
 
     test = AGM()
 
-    test.agm_inclusion(belief_base_test, phi_test)
+    print(f"succes: {test.agm_success(belief_base_test, phi_test)}")
+    print(f"inclusion: {test.agm_inclusion(belief_base_test, phi_test)}")
+    print(f"vacuity: {test.agm_vacuity(belief_base_test, phi_test)}")
+    print(f"extensionality: {test.agm_extensionality(belief_base_test, phi_test)}")
+
+    # print(test.find_phi_in_base(belief_base_test, phi_test))
+    # test.agm_vacuity(belief_base_test, phi_test)
+    # print(test.agm_vacuity(belief_base_test, phi_test)) 
+
+    # belief_base_test_exp = belief_base_test.copy()
+    # belief_base_test_exp.append(phi_test)
+
+    # assert test.compare_bases(belief_base_test, belief_base_test_exp) == True
 
 
 if __name__ == "__main__":
